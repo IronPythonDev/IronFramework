3b92e500f078a12e5faaf7a16b9e694b3b879c42
Vlad Kovalchuk <60366174+IronPythonDev@users.noreply.github.com>
2022-06-27 04:05:17 +0300

    Created ps script for creating plugins and for including they to core

--- src/Core/Server/IronFramework.Core.Server/IronCoreResource.cs
+++ src/Core/Server/IronFramework.Core.Server/IronCoreResource.cs
@@ -3,21 +3,24 @@ using AltV.Net.Async;
 using AltV.Net.EntitySync;
 using AltV.Net.EntitySync.ServerEvent;
 using AltV.Net.EntitySync.SpatialPartitions;
+using IronFramework.Core.Plugin;
 using IronFramework.Core.Server.Controllers.Interaction;
 using IronFramework.Core.Server.Controllers.TextLabel;
 
 namespace IronFramework.Core.Server
 {
-    public class IronCoreResource : AsyncResource
+    public partial class IronCoreResource : AsyncResource
     {
+        public static IList<IronPlugin> IncludedPlugins { get; set; } = new List<IronPlugin>();
+
         public override void OnStart()
         {
             AltEntitySync.Init(
-                5, 
-                (syncrate) => 100, 
-                (threadId) => false, 
-                (threadCount, repository) => new ServerEventNetworkLayer(threadCount, repository), 
-                (entity, threadCount) => entity.Type, 
+                5,
+                (syncrate) => 100,
+                (threadId) => false,
+                (threadCount, repository) => new ServerEventNetworkLayer(threadCount, repository),
+                (entity, threadCount) => entity.Type,
                 (entityId, entityType, threadCount) => entityType,
                 (threadId) => threadId switch
                 {
@@ -39,13 +42,17 @@ namespace IronFramework.Core.Server
                 .AddPosition(new AltV.Net.Data.Position(0, 0, 71))
                 .AddHandler((p) => Alt.Log($"Interaction: {p.Name}")));
 
-            TextLabelController.Add(new Shared.Controllers.World3DTextLabel("Vlad1234", new System.Numerics.Vector3(0, 0, 71)) { Scale = 0.4f } );
+            TextLabelController.Add(new Shared.Controllers.World3DTextLabel("Vlad1234", new System.Numerics.Vector3(0, 0, 71)) { Scale = 0.4f });
             TextLabelController.Add(new Shared.Controllers.World3DTextLabel("Simple Text", new System.Numerics.Vector3(-12.519726753234863f, 5.703808307647705f, 72f), range: 5) { Scale = 0.4f });
+
+            foreach (var method in typeof(IronCoreResource).GetMethods().Where(p => p.Name.Contains("_Plugin"))) method.Invoke(this, null);
+
+            foreach (var plugin in IncludedPlugins) plugin.OnStart();
         }
 
         public override void OnStop()
         {
-            
+
         }
     }
 }
